3565d383ec2420a57e26c9849ce64171b9bcc5f3
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-23 15:24:11 -0400

    finished up user js

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,4 +1,4 @@
-const { Schema, model, default: mongoose, get } = require('mongoose');
+const { Schema, model} = require('mongoose');
 
 const reactionSchema = new Schema (
     {
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const { getMaxListeners } = require('./Thoughts');
+const { Thoughts } = require('.');
+
+const userSchema = new Schema (
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            maxLength: 50
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            maxLength: 50,
+        },
+        Thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+        friends: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals:true,
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(function (){
+    return this.friends.length;
+});
+
+const user = model('User', userSchema);
+
+module.exports = User;
